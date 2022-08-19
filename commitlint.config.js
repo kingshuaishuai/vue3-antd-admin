cc9cf252a6acfae3840fd7bd28c3ca7231d533dd
kingshuaishuai <ken.wang@mrs.ai>
2022-08-20 02:03:27 +0800

    feat(custom): 添加husky与commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  'extends': [
+    '@commitlint/config-conventional',
+  ],
+  'roles': {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'revert',
+        'WIP',
+        'build',
+      ],
+    ],
+  },
+  'subject-case': [0],
+};
